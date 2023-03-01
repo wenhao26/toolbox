e699819c278876f8b14d33ea582f848c268af100
Wu Wenhao <1321471639@qq.com>
2023-03-01 12:00:24 +0800

    changes

--- centrifugo/centrifugo.go
+++ centrifugo/centrifugo.go
@@ -6,17 +6,20 @@ import (
 	"github.com/centrifugal/gocent/v3"
 )
 
+// Connection configuration
 type Option struct {
 	Addr string
 	Key  string
 }
 
+// Serve structure
 type Serve struct {
 	Cent *gocent.Client
 }
 
 var ctx = context.Background()
 
+// Create Centrifugal Serve instance
 func NewServe(option *Option) *Serve {
 	client := gocent.New(gocent.Config{
 		Addr: option.Addr,
@@ -25,10 +28,25 @@ func NewServe(option *Option) *Serve {
 	return &Serve{Cent: client}
 }
 
+// Publish message to channel
 func (s *Serve) Publish(channel string, body []byte) (gocent.PublishResult, error) {
 	result, err := s.Cent.Publish(ctx, channel, body)
 	if err != nil {
 		return gocent.PublishResult{}, err
 	}
 	return result, nil
+}
+
+// Publish message to pipe
+func (s *Serve) PipePublish(channel string, data [][]byte) ([]gocent.Reply, error) {
+	pipe := s.Cent.Pipe()
+
+	for _, v := range data {
+		_ = pipe.AddPublish(channel, v)
+	}
+	replies, err := s.Cent.SendPipe(ctx, pipe)
+	if err != nil {
+		return nil, err
+	}
+	return replies, nil
 }
