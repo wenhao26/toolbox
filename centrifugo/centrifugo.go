566fe7baf3e26ca87920ae2aa05883e913d6dd50
Wu Wenhao <1321471639@qq.com>
2023-03-07 14:12:37 +0800

    add centrifugo gen token function

--- centrifugo/centrifugo.go
+++ centrifugo/centrifugo.go
@@ -2,19 +2,25 @@ package centrifugo
 
 import (
 	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"strings"
 
 	"github.com/centrifugal/gocent/v3"
 )
 
 // Connection configuration
 type Option struct {
-	Addr string
-	Key  string
+	Addr   string
+	ApiKey string
 }
 
 // Serve structure
 type Serve struct {
-	Cent *gocent.Client
+	Cent   *gocent.Client
+	Secret string
 }
 
 var ctx = context.Background()
@@ -23,9 +29,87 @@ var ctx = context.Background()
 func NewServe(option *Option) *Serve {
 	client := gocent.New(gocent.Config{
 		Addr: option.Addr,
-		Key:  option.Key,
+		Key:  option.ApiKey,
 	})
 	return &Serve{Cent: client}
+}
+
+// Set token key
+func (s *Serve) SetSecret(secret string) *Serve {
+	s.Secret = secret
+	return s
+}
+
+// Generate connection JWT
+func (s *Serve) GenConnToken(uid string, exp int, info, channels interface{}) string {
+	header := map[string]string{
+		"typ": "JWT",
+		"alg": "HS256",
+	}
+	payload := map[string]interface{}{
+		"sub": uid,
+	}
+
+	if exp > 0 {
+		payload["exp"] = exp
+	}
+	if info != nil {
+		payload["info"] = info
+	}
+	if channels != nil {
+		payload["channels"] = channels
+	}
+
+	var segments []string
+	headerJson, _ := json.Marshal(header)
+	headerEncodeStr := urlSafeB64Encode(headerJson)
+	segments = append(segments, headerEncodeStr)
+
+	payloadJson, _ := json.Marshal(payload)
+	payloadEncodeStr := urlSafeB64Encode(payloadJson)
+	segments = append(segments, payloadEncodeStr)
+
+	signingInput := strings.Join(segments, ".")
+	signature := sign(signingInput, []byte(s.Secret))
+	signatureStr := urlSafeB64Encode(signature)
+	segments = append(segments, signatureStr)
+
+	return strings.Join(segments, ".")
+}
+
+// Generate private channel JWT
+func (s *Serve) GenPrivateChannelToken(client, channel string, exp int, info interface{}) string {
+	header := map[string]string{
+		"typ": "JWT",
+		"alg": "HS256",
+	}
+	payload := map[string]interface{}{
+		"client":  client,
+		"channel": channel,
+	}
+
+	if exp > 0 {
+		payload["exp"] = exp
+	}
+	if info != nil {
+		payload["info"] = info
+	}
+
+	var segments []string
+	headerJson, _ := json.Marshal(header)
+	headerEncodeStr := urlSafeB64Encode(headerJson)
+	segments = append(segments, headerEncodeStr)
+
+	payloadJson, _ := json.Marshal(payload)
+	payloadEncodeStr := urlSafeB64Encode(payloadJson)
+	segments = append(segments, payloadEncodeStr)
+
+	signingInput := strings.Join(segments, ".")
+	signature := sign(signingInput, []byte(s.Secret))
+	signatureStr := urlSafeB64Encode(signature)
+	segments = append(segments, signatureStr)
+
+	return strings.Join(segments, ".")
 }
 
 // Publish message to channel
@@ -49,4 +133,17 @@ func (s *Serve) PipePublish(channel string, data [][]byte) ([]gocent.Reply, erro
 		return nil, err
 	}
 	return replies, nil
+}
+
+func urlSafeB64Encode(input []byte) string {
+	str := base64.URLEncoding.EncodeToString(input)
+	str = strings.Replace(str, "+/", "-_", -1)
+	str = strings.Replace(str, "=", "", -1)
+	return str
+}
+
+func sign(cipherText string, key []byte) []byte {
+	h := hmac.New(sha256.New, key)
+	h.Write([]byte(cipherText))
+	return h.Sum(nil)
 }
--- example/cent.go
+++ example/cent.go
@@ -11,10 +11,19 @@ import (
 
 func main() {
 	cent := centrifugo.NewServe(&centrifugo.Option{
-		Addr: "http://127.0.0.1:9501/api",
-		Key:  "99e2a129-e85d-4cfc-bdd4-250df0525165",
+		Addr:   "http://127.0.0.1:9501/api",
+		ApiKey: "99e2a129-e85d-4cfc-bdd4-250df0525165",
 	})
 
+	// 生成连接JWT
+	//tokenSecretKey := "5c1b094c-dc02-43e4-8cd8-0a3d08531112"
+	//token1 := cent.SetSecret(tokenSecretKey).GenConnToken("1688", 0, nil, nil)
+	//fmt.Println(token1)
+	////token2 := cent.SetSecret(tokenSecretKey).GenPrivateChannelToken("1688", "test-channel", 0, nil)
+	////fmt.Println(token2)
+	//os.Exit(0)
+
+	// 发布消息
 	channel := "test-channel"
 	for {
 		body := map[string]string{
